72c60d993af5cda79c47c780f002b5affabc3e05
Roc Taylor <rocrtaylor@gmail.com>
2022-02-15 10:49:49 -0600

    end of crud, begin css

--- models/monstersPerm.js
+++ models/monstersPerm.js
@@ -1,3 +1,16 @@
-const monstersPermanent = [{ name: 'Abominable Snow Turle', image: '/image/snowturtle.png', story: 'Wandering the snowy hills looking for romaine' }, { name: 'Swamp Bunny', image: '/image/swampy.jpeg', story: 'Roming southern Louisiana in search of beer' }]
+const monstersPermanent = [{ name: 'Abominable Snow Turle', image: '/image/snowturtle.png', story: 'Wandering the snowy hills looking for romaine and beer', location:'Mountains' },
+    { name: 'Swamp Bunny', image: '/image/swampBunny.jpeg', story: 'Roming southern Louisiana in search of beer' , location: 'Swamp'},
+    { name: 'Blender', image: '/image/Bender.png', story: 'Blender is the lesser known Twin of Bender. Bender modified Blender into a Margarita Machine', location: 'Space' },
+    { name: 'Ernie McKraken', image: '/image/ernie.png', story: 'Being the cousin of the famous kraken is diffucult when you have the low end of the gene pool. Unlike his Octopus cousin, Ernie is a jellyfish with No tentacle strength', location: 'The Deep' },
+    { name: 'Stumpy', image: '/image/bigfoot.png', story: 'Stumpy is Bigfoots little brother that ate too many wild mushrooms and now just stares at the sun', location: 'Mountains' },
+    { name: 'Chinchillacabra', image: '/image/chin.jpeg', story: 'Chin as he is known by locals wanders the desert insearch of beer and tacos', location: 'South West' },
+{name: 'Lochness Lobster', image: '/image/lobster.png', story: 'Long being trapped in his cave by that jerk nessy, very few sightings have been reported. Only known fear is butter', location: 'The Loch'}]
+
+
+
+
+
+
+
 
 module.exports = monstersPermanent
